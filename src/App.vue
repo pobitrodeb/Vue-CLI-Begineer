b71c990cda8119fb59efe66a428de5931cd8adac
Pobitrodeb <pobitrodn@gmail.com>
2023-01-18 22:47:33 +0600

    lession 32 successfully done

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,28 @@
 <template>
     <h2>{{ message }}</h2>
-   <contact-details> </contact-details>
+   <contact-details
+    name="Pobitro Debnath"
+    website-address="www.pobitrodeb.com"
+    mobile-nmber="233043311"
+   > </contact-details>
+
+   <contact-details
+    name="Rahul Das "
+    website-address="www.rahul.com"
+    mobile-nmber="01212124"
+   > </contact-details>
+
+   <contact-details
+    name="Anup "
+    website-address="www.anup.com"
+    mobile-nmber ="0721521231"
+   > </contact-details>
+
 </template>
 
 <script>
 export default {
+    
     data() {
         return {
             message: "Test App Successfully Run "
--- src/ContactDetail.vue
+++ src/ContactDetail.vue
@@ -1,23 +1,28 @@
 <template>
         <div class="contact-info">
-            <h1> Contact Info </h1>   
+            <h1> Contact Info </h1>  
+            <h2>name : {{ name }}</h2> 
+            <h3>website : {{ websiteAddress }}</h3> 
+            <h4>mobile : {{ mobileNumber }}</h4> 
         </div>
 </template>
 
 <script>
 
 export default({
+    props: ['name', 'websiteAddress', 'mobileNumber'],
     data() {
-        return {
-            name: "Pobitro Debnath"
-        };
+        
+        // return {
+        //     name: "Pobitro Debnath"
+        // };
     },
   
 });
 </script>
 <style scoped>
     .contact-info{
-        color: red;
-        font-size: 75px;
+        color: rgb(51, 51, 51);
+        font-size: 15px;
     }
 </style>
