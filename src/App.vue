63b4967ffa97ec9009b46fa575ac17bb48d136e0
Pobitrodeb <pobitrodn@gmail.com>
2023-01-18 21:09:05 +0600

    lesson 28 successfully done

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <h2>{{ message }}</h2>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: "Test App Successfully Run "
+        }
+    },
+   
+}
+</script>
+
+<style>
+
+</style>
